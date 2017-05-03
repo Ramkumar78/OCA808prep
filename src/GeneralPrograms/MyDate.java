1cfaafbb090e46703c90a8bb1a9c243a770a6345
shriram2222@gmail.com <shriram2222@gmail.com>
2017-05-03 15:01:55 +0100

    Test2

---
+++ src/GeneralPrograms/MyDate.java
@@ -0,0 +1,25 @@
+package GeneralPrograms;
+
+public class MyDate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public int day;
+	public int month;
+	public int year;
+	public MyDate(int date, int month, int year){
+		
+	}
+	public MyDate(){}
+	public void setDate1(int i, int j, int k) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void setDate(int i, int j, int k) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
